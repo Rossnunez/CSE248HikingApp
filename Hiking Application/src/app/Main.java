bee5fa66bc3df9637513dfeb8ce50060bec6a5f7
Rosseleen Nunez <nrosseleen@gmail.com>
2021-01-30 19:29:05 -0500

    Adding appropiate data structures for users,trails,hikinghistory

--- Hiking Application/src/app/Main.java
+++ Hiking Application/src/app/Main.java
@@ -1,26 +1,35 @@
 package app;
 	
+import java.util.HashSet;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
+import model.HikingHistory;
+import model.Trail;
+import model.User;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
-	@Override
+	private static TreeMap<String, User> userMap;
+	private static HashSet<Trail> trailSet;
+	
 	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+		
 	}
 	
 	public static void main(String[] args) {
 		launch(args);
+	}
+	
+	public static TreeMap<String,User> getUserMap(){
+		return userMap;
+	}
+	
+	public static HashSet<Trail> getTrailSet(){
+		return trailSet;
 	}
 }
--- Hiking Application/src/model/User.java
+++ Hiking Application/src/model/User.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.TreeSet;
+
 public class User {
 	private String username;
 	private String password;
@@ -8,11 +10,10 @@ public class User {
 	private String phoneNumber;
 	private String image;
 	private AccountType accountType;
-	private HikingHistory hikingHistory;
-	
-	
-	public User(String username, String password, String firstName, String lastName, String phoneNumber,
-			String image, AccountType accountType, HikingHistory hikingHistory) {
+	private TreeSet<HikingHistory> hikingHistorySet;
+
+	public User(String username, String password, String firstName, String lastName, String phoneNumber, String image,
+			AccountType accountType, TreeSet<HikingHistory> hikingHistorySet) {
 		super();
 		this.username = username;
 		this.password = password;
@@ -21,65 +22,78 @@ public class User {
 		this.phoneNumber = phoneNumber;
 		this.image = image;
 		this.accountType = accountType;
-		this.hikingHistory = hikingHistory;
+		this.hikingHistorySet = hikingHistorySet;
 	}
-	
+
+	public TreeSet<HikingHistory> getHikingHistorySet() {
+		return hikingHistorySet;
+	}
+
+	public void setHikingHistorySet(TreeSet<HikingHistory> hikingHistorySet) {
+		this.hikingHistorySet = hikingHistorySet;
+	}
+
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
 	public String getPhoneNumber() {
 		return phoneNumber;
 	}
+
 	public void setPhoneNumber(String phoneNumber) {
 		this.phoneNumber = phoneNumber;
 	}
+
 	public String getImage() {
 		return image;
 	}
+
 	public void setImage(String image) {
 		this.image = image;
 	}
-	public HikingHistory getHikingHistory() {
-		return hikingHistory;
-	}
-	public void setHikingHistory(HikingHistory hikingHistory) {
-		this.hikingHistory = hikingHistory;
-	}
-
 
 	public AccountType getAccountType() {
 		return accountType;
 	}
-	
+
 	public void setAccountType(AccountType accountType) {
-		this.accountType = accountType;	
+		this.accountType = accountType;
 	}
 
 	@Override
 	public String toString() {
-		return "UserInfo [username=" + username + ", password=" + password + ", firstName=" + firstName + ", lastName="
+		return "User [username=" + username + ", password=" + password + ", firstName=" + firstName + ", lastName="
 				+ lastName + ", phoneNumber=" + phoneNumber + ", image=" + image + ", accountType=" + accountType
-				+ ", hikingHistory=" + hikingHistory + "]";
+				+ ", hikingHistorySet=" + hikingHistorySet + "]";
 	}
+
 }
