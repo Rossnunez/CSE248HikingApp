d9615b1dfd3cbc5d287b3bd82bf74d9a10c5b4ce
Rosseleen Nunez <nrosseleen@gmail.com>
2021-02-01 14:22:34 -0500

    Testing the CheckComboBox function with the difficulty level

--- Hiking Application/src/controller/Controller2.java
+++ Hiking Application/src/controller/Controller2.java
@@ -4,13 +4,19 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.ResourceBundle;
 import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 import javax.imageio.ImageIO;
+
+import org.controlsfx.control.CheckComboBox;
 
 import app.Main;
 import javafx.collections.FXCollections;
@@ -41,6 +47,8 @@ import javafx.stage.FileChooser;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.HikeType;
+import model.HikingHistory;
+import model.Level;
 import model.Trail;
 import model.User;
 
@@ -65,9 +73,14 @@ public class Controller2 implements Initializable {
 	public ImageView imageField;
 	public Image defaultImage;
 	public BufferedImage bufferedImage;
+	
 
 	// start trails tab
-	static HikeType answer;
+	public CheckComboBox<String> difficultyBox;
+	
+	static HikeType answerType;
+	static Level answerLevel;
+	
 	public Text selectedTrail;
 	public ComboBox<String> sortByBox;
 	public ObservableList<String> searchList = FXCollections.observableArrayList("NAME", "LENGTH", "EVALATION", "LEVEL",
@@ -82,13 +95,80 @@ public class Controller2 implements Initializable {
 	public TableColumn<Trail, String> type;
 	public TextField searchField;
 
-	// start trail tab//
 	
-	public void startTrail(ActionEvent event) {
+	//hiking history tab//
+	public void searchForTrailsHistory(ActionEvent event) {
 		
 	}
 	
-	public static void display() {
+	public void completeTrail(ActionEvent event) {
+		
+	}
+	
+	public void selectedTrailHistory(MouseEvent event) {
+		
+	}
+	// start trail tab//
+	public void selectedTrail(MouseEvent event) {
+		try {
+			selectedTrail.setText(table.getSelectionModel().getSelectedItem().getTrailName());
+		} catch (NullPointerException e) {
+			//
+		}
+	}
+	
+	public void startTrail(ActionEvent event) {
+		Trail trail = table.getSelectionModel().getSelectedItem();
+		SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/YYYY hh:mm:ss");
+		Date currentTime = new Date(System.currentTimeMillis());
+		String current = sdf.format(currentTime);
+		TreeSet<String> images = new TreeSet<String>();
+		HikingHistory hikingHistory = new HikingHistory(trail.getTrailAddress(), current, "In Progress", trail.getMiles(), "In Progress", images, "In Progress");
+		user.getHikingHistorySet().add(hikingHistory);
+		table.getItems().remove(table.getSelectionModel().getSelectedItem());
+		
+		
+	}
+	
+	public static void displayLevel() {
+		Stage window = new Stage();
+		window.initModality(Modality.APPLICATION_MODAL);
+		window.setTitle("Types of Levels");
+		window.setMinWidth(300);
+		window.setMinHeight(250);
+		Label label = new Label();
+		label.setText("Select a Trail Level");
+		label.setAlignment(Pos.TOP_CENTER);
+		
+		Button easyButton = new Button("Hard Difficulty");
+		Button modButton = new Button("Moderate Difficulty");
+		Button hardButton = new Button("Easy Difficulty");
+		
+		easyButton.setOnAction(e ->{
+			answerLevel = Level.EASY;
+			window.close();
+		});
+		
+		modButton.setOnAction(e ->{
+			answerLevel = Level.MODERATE;
+			window.close();
+		});
+		
+		hardButton.setOnAction(e ->{
+			answerLevel = Level.HARD;
+			window.close();
+		});
+		
+		VBox layout = new VBox(10);
+		layout.getChildren().addAll(label, easyButton, modButton, hardButton);
+		layout.setAlignment(Pos.CENTER);
+		
+		Scene scene = new Scene(layout);
+		window.setScene(scene);
+		window.showAndWait();
+	}
+	
+	public static void displayType() {
 		Stage window = new Stage();
 		window.initModality(Modality.APPLICATION_MODAL);
 		window.setTitle("Types of Trails");
@@ -103,17 +183,17 @@ public class Controller2 implements Initializable {
 		Button pointButton = new Button("Point Trail");
 		
 		loopButton.setOnAction(e ->{
-			answer = HikeType.LOOP;
+			answerType = HikeType.LOOP;
 			window.close();
 		});
 		
 		outButton.setOnAction(e ->{
-			answer = HikeType.OUT_AND_BACK;
+			answerType = HikeType.OUT_AND_BACK;
 			window.close();
 		});
 		
 		pointButton.setOnAction(e ->{
-			answer = HikeType.POINT_TO_POINT;
+			answerType = HikeType.POINT_TO_POINT;
 			window.close();
 		});
 		
@@ -148,14 +228,17 @@ public class Controller2 implements Initializable {
 			trail.addAll(result);
 			table.setItems(trail);
 		} else if(type.contentEquals("TYPE")) {
+			displayType();
 			List<Trail> result = trailSet.stream()
-					.filter(trail -> trail.getType().equals(HikeType.LOOP)).collect(Collectors.toList());
+					.filter(trail -> trail.getType().equals(answerType)).collect(Collectors.toList());
 			trail.addAll(result);
 			table.setItems(trail);
 		} else if(type.contentEquals("LEVEL")) {
-			display();
-			//List<Trail> result = trailSet.stream()
-			//		.filter(trail -> String)
+			displayLevel();
+			List<Trail> result = trailSet.stream()
+					.filter(trail -> trail.getDifficulty().equals(answerLevel)).collect(Collectors.toList());
+			trail.addAll(result);
+			table.setItems(trail);
 		}
 	}
 
@@ -167,7 +250,7 @@ public class Controller2 implements Initializable {
 		this.setFilePath(fileChooser.showOpenDialog(stage));
 
 		try {
-			image = filePath.getPath();
+			//image = filePath.getPath();
 			BufferedImage bufferedImage = ImageIO.read(filePath);
 			Image image = SwingFXUtils.toFXImage(bufferedImage, null);
 			imageField.setImage(image);
@@ -237,27 +320,32 @@ public class Controller2 implements Initializable {
 
 		File file = new File(Controller1.getUser().getImage());
 		setFilePath(file);
-
+		
+		//update user tab
 		try {
 			bufferedImage = ImageIO.read(filePath);
 			Image image = SwingFXUtils.toFXImage(bufferedImage, null);
 			imageField.setImage(image);
 		} catch (IOException e) {
 			defaultImage = imageField.getImage();
-			image = "\\RawData\\user.png";
+			//image = "\\RawData\\user.png";
 		}
-		// Image image = SwingFXUtils.toFXImage(bufferedImage, null);
-		// imageField.setImage(image);
-
+		
 		username = Controller1.getUsername();
 		user = Controller1.getUser();
 
-		// update tab
 		usernameText.setText(username);
 		passwordField.setText(user.getPassword());
 		firstnameField.setText(user.getFirstName());
 		lastnameField.setText(user.getLastName());
 		phonenumberField.setText(user.getPhoneNumber());
+		
+		final ObservableList<String> strings = FXCollections.observableArrayList();
+		strings.add("HARD");
+		strings.add("MODERATE");
+		strings.add("EASY");
+		
+		difficultyBox.getItems().addAll(strings);
 
 		// start trails tab
 		trail = FXCollections.observableArrayList();
